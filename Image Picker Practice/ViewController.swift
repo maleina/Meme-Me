e2a1c916dc21a35c61f95d5b9c64727dbe0cd3c2
Maleina Bidek <maleina@ucla.edu>
2021-03-20 13:09:21 -0400

    Initial Commit

---
+++ Image Picker Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image Picker Practice
+//
+//  Created by Maleina Bidek on 3/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
