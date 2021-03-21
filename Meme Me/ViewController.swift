52fbe2d2aa910b429ad7f23f6f7661611040d31f
Maleina Bidek <maleina@ucla.edu>
2021-03-21 14:31:16 -0400

    feat: Add text attributes to meme text

--- Meme Me/ViewController.swift
+++ Meme Me/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     let memeTextFieldDelegate = MemeTextFieldDelegate()
     
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth:  -5.0
+    ]
+    
     override func viewWillAppear(_ animated: Bool) {
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
@@ -25,9 +32,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         topTextField.text = "TOP"
         topTextField.textAlignment = .center
         topTextField.delegate = memeTextFieldDelegate
+        topTextField.defaultTextAttributes = memeTextAttributes
         bottomTextField.text = "BOTTOM"
         bottomTextField.textAlignment = .center
         bottomTextField.delegate = memeTextFieldDelegate
+        bottomTextField.defaultTextAttributes = memeTextAttributes
         
     }
 
