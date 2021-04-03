436a23d31be5d68785b19fdda83e90b9eed7cbf0
Maleina Bidek <maleina@ucla.edu>
2021-04-03 12:59:06 -0400

    feat: Add initial structure for collection view

---
+++ Meme Me/SentMemesCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SentMemesCollectionViewCell.swift
+//  Meme Me
+//
+//  Created by Maleina Bidek on 4/3/21.
+//
+
+import UIKit
+
+class SentMemesCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Meme Me/SentMemesCollectionViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SentMemesCollectionViewController.swift
+//  Meme Me
+//
+//  Created by Maleina Bidek on 4/3/21.
+//
+
+import Foundation
+import UIKit
+
+private let reuseIdentifier = "SentMemesCollectionViewCell"
+
+class SentMemesCollectionViewController: UICollectionViewController {
+    
+    // MARK: Properties
+    var memes: [Meme] = (UIApplication.shared.delegate as! AppDelegate).memes
+    
+    
+    // MARK: Life Cycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.memes.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
+        
+    }
+
+
+}
