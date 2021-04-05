8866e80be58d71a524c82258729fa51a4dc6d90a
Maleina Bidek <maleina@ucla.edu>
2021-04-05 16:03:13 -0400

    feat: Populate collection view with send memes

--- Meme Me/SentMemesCollectionViewCell.swift
+++ Meme Me/SentMemesCollectionViewCell.swift
@@ -8,5 +8,5 @@
 import UIKit
 
 class SentMemesCollectionViewCell: UICollectionViewCell {
-    
+    @IBOutlet weak var customImageView: UIImageView!
 }
--- Meme Me/SentMemesCollectionViewController.swift
+++ Meme Me/SentMemesCollectionViewController.swift
@@ -13,27 +13,31 @@ private let reuseIdentifier = "SentMemesCollectionViewCell"
 class SentMemesCollectionViewController: UICollectionViewController {
     
     // MARK: Properties
-    var memes: [Meme] = (UIApplication.shared.delegate as! AppDelegate).memes
+    var memes: [Meme]! {
+        let object = UIApplication.shared.delegate
+        let appDelegate = object as! AppDelegate
+        return appDelegate.memes
+    }
     
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
     
     // MARK: Life Cycle
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.tabBarController?.tabBar.isHidden = false
-        collectionView!.reloadData()
+        collectionView.reloadData()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        
+        let space:CGFloat = 3.0
+        let width = (view.frame.size.width - (2 * space)) / 3.0
+        let height = (view.frame.size.height - (2 * space)) / 3.0
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: width, height: height)
     }
 
     // MARK: UICollectionViewDataSource
@@ -43,16 +47,15 @@ class SentMemesCollectionViewController: UICollectionViewController {
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! SentMemesCollectionViewCell
+        let meme = self.memes[(indexPath as NSIndexPath).row]
+        
+        cell.customImageView.image = meme.memedImage
+        
         return cell
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
         
     }
-
-
 }
