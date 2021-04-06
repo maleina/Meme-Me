205a2d874b120ce068fcc1c95b492940ae65d24d
Maleina Bidek <maleina@ucla.edu>
2021-04-06 12:03:39 -0400

    style: Use dot notion when defining memeTextAttributes

--- Meme Me/MemeEditorViewController.swift
+++ Meme Me/MemeEditorViewController.swift
@@ -28,10 +28,10 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     // MARK: Constants
     // Dictionary of meme text attributes
     let memeTextAttributes: [NSAttributedString.Key: Any] = [
-        NSAttributedString.Key.strokeColor: UIColor.black,
-        NSAttributedString.Key.foregroundColor: UIColor.white,
-        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-        NSAttributedString.Key.strokeWidth:  -5.0
+        .strokeColor: UIColor.black,
+        .foregroundColor: UIColor.white,
+        .font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        .strokeWidth:  -5.0
     ]
     
     // MARK: Life cycle methods
